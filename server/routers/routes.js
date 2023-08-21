8e59bfec2dcd3754aca8222a2948c25c06075a83
Woobae <kim@Woobaes-MacBook-Pro.local>
2023-08-21 09:17:17 -0700

    set up middlewares

--- server/routers/routes.js
+++ server/routers/routes.js
@@ -4,20 +4,23 @@ const socialControllers = require('../controllers/socialControllers.js');
 const router = express.Router();
 
 router.post('/signup', socialControllers.signup, (req, res) => {
-    console.log('in router, after signup')
-    return res.status(200);
+  const {id} = res.locals;
+    return res.status(200).redirect(`GET /:${id}`);
     // .redirect("/home"); 
 });
 router.post('/login', socialControllers.login, socialControllers.startSession, (req, res) => {
-    return res.status(302).redirect("/home"); 
+    const {id} = res.locals;
+    return res.status(302).redirect(`GET /:${id}`); 
 });
 //user's homepage has the ID of server info stored as type
-router.post('/:id', (req, res) => {
-  //need to hash ID for this
+router.get('/:id', socialControllers.isLoggedIn, socialControllers.pageDetails, (req, res) => {
+  const {profile} = res.locals.profile;
+  return res.status(200).send(profile);
 });
 
-router.post('/', (req, res) => {
-  
-});
+router.post('/:id/post', socialControllers.isLoggedIn, socialControllers.textpost, (req, res) => {
+  const {content}=res.locals.content;
+  return res.status(200).send(content);
+})
 
 module.exports = router;
